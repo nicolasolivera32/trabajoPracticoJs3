2e273559858b399728d34d6cdab129a89a7f3f04
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-11-10 18:36:55 -0300

    se realizó una función donde se pide un número y te dice si es par o impar

---
+++ js/ejercicio4TP3.js
@@ -0,0 +1,21 @@
+/*
+Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+*/
+
+let valor = 0
+
+function numero(ingreseNumero){
+    if(ingreseNumero % 2 === 0){
+        document.write(`${ingreseNumero} <h2>es un número par</h2>`)
+    }else{
+        document.write(`${ingreseNumero} <h2>es un número impar</h2>`)
+    }
+}
+
+valor = parseInt(prompt("ingrese un número"))
+if(valor >0 && Number.isInteger(valor)){
+    numero(valor);
+}else{
+    document.write(`<h2>Es un número inválido</h2>`)
+}
+
