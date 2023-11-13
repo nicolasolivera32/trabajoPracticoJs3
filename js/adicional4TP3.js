43cff8ed61581140f00832c59a0b9a81ed954548
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-11-13 01:47:49 -0300

    se comenzo a realizar el ejercicio adicional 4 de trello

---
+++ js/adicional4TP3.js
@@ -0,0 +1,33 @@
+/*
+Generar números primos: Escribe una función que tome un número entero como argumento y devuelva un arreglo de todos los números primos menores o iguales a ese número.
+*/
+
+// const numero = parseInt(prompt("Ingrese un número"));
+// const prueba = [];
+
+// function numerosPrimos(valor) {
+//   if ((valor = Number.isInteger(valor) && valor >= 2)) {
+//     for (let i = 2; i < valor; i++) {
+//       if (valor % i === 0) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// function unionValoresPrimos (conjunto){
+//   for(i=2; i<conjunto;i++){
+//     if(numerosPrimos(i)){
+//       prueba[i]
+//     }
+//   }
+// }
+// unionValoresPrimos(numero)
+
+
+// NO PUDE RESOLVER EL EJERCICIO
+
+
+
+
